03b578eb1b4a82d8d13e455a92cd453163cfa68c
papppsanyi <papppsanyi@yahoo.com>
2019-08-19 15:35:02 +0200

    Create myPortalFucker.js

---
+++ myPortalFucker.js
@@ -0,0 +1,13 @@
+if (document.getElementById("ctl00_PlaceHolderPageDescription_WPVykaz1_ctl00_NEW_ProjektDdl").selectedIndex == 0){
+	document.getElementById("ctl00_PlaceHolderPageDescription_WPVykaz1_ctl00_NEW_ProjektDdl").selectedIndex = 2;
+	document.forms[0].submit();
+}
+else if (document.getElementById("ctl00_PlaceHolderPageDescription_WPVykaz1_ctl00_NEW_ProjektDdl").selectedIndex == 1){
+	
+}
+else{
+	document.getElementById("ctl00_PlaceHolderPageDescription_WPVykaz1_ctl00_NEW_UlohaDdl").selectedIndex = 1;
+document.getElementById("ctl00_PlaceHolderPageDescription_WPVykaz1_ctl00_NEW_TypPraceDdl").selectedIndex = 27;
+document.getElementById("ctl00_PlaceHolderPageDescription_WPVykaz1_ctl00_NEW_HodinyDdl").selectedIndex = 8;
+document.getElementById('ctl00_PlaceHolderPageDescription_WPVykaz1_ctl00_NEW_PopisTb').value = 'PFS - testing';
+}
